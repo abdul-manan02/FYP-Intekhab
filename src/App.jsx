4b795b131baf221ae85ff34cff837fbc8a19a2c5
Bilal Khan <bkhan8423@gmail.com>
2023-09-18 21:12:52 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <>
+            <div className="text-red-300 text-4xl">
+                <h1>hello</h1>
+            </div>
+        </>
+    );
+}
+
+export default App;
