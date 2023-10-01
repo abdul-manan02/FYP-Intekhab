45d3590fb4279fe747c3beee6e46e1c5883b7471
Bilal Khan <bkhan8423@gmail.com>
2023-10-02 01:10:12 +0500

    added react-router-dom and setup routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Register } from './features/register/register';
 import { Login } from './features/login/login';
+
+// * Once we have auth setup and integrated, we can protect our routes like this
+// function PrivateRoute({ element, ...rest }) {
+//     return isAuthenticated ? <Route {...rest} element={element} /> : <Navigate to="/login" />;
+// }
+
 function App() {
     return (
-        <>
-            <Login />
-        </>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/signup" element={<Register />} />
+            </Routes>
+        </Router>
     );
 }
 
