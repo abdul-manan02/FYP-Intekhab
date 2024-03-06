bb7745b0588a75f65f8b514353451afad845ace6
Shoaib Ali <shoaibali209@gmail.com>
2024-03-06 15:00:42 +0500

    Added new ABI and Adress

--- src/config.js
+++ src/config.js
@@ -1,21 +1,65 @@
-export const STORAGE_ADDRESS = '0xa304295bB04Ae6FE1Db6A739352da52415199570';
-export const STORAGE_ABI = [
+export const EVOTING_ADDRESS = '0x957e76713ec7107354a0bCa69ecE3AaB26568222';
+export const EVOTING_ABI = [
+    {
+        inputs: [],
+        stateMutability: 'nonpayable',
+        type: 'constructor',
+    },
+    {
+        anonymous: false,
+        inputs: [
+            {
+                indexed: true,
+                internalType: 'address',
+                name: 'voter',
+                type: 'address',
+            },
+            {
+                indexed: false,
+                internalType: 'enum EVoting.Party',
+                name: 'party',
+                type: 'uint8',
+            },
+        ],
+        name: 'VoteCast',
+        type: 'event',
+    },
     {
         anonymous: false,
         inputs: [
             {
                 indexed: false,
+                internalType: 'string',
+                name: 'winner',
+                type: 'string',
+            },
+            {
+                indexed: false,
                 internalType: 'uint256',
-                name: 'newValue',
+                name: 'votes',
                 type: 'uint256',
             },
         ],
-        name: 'ValueUpdated',
+        name: 'WinnerDeclared',
         type: 'event',
     },
     {
         inputs: [],
-        name: 'storedValue',
+        name: 'admin',
+        outputs: [
+            {
+                internalType: 'address',
+                name: '',
+                type: 'address',
+            },
+        ],
+        stateMutability: 'view',
+        type: 'function',
+        constant: true,
+    },
+    {
+        inputs: [],
+        name: 'endTime',
         outputs: [
             {
                 internalType: 'uint256',
@@ -29,19 +73,93 @@ export const STORAGE_ABI = [
     },
     {
         inputs: [],
-        name: 'storeHardcodedValue',
+        name: 'startTime',
+        outputs: [
+            {
+                internalType: 'uint256',
+                name: '',
+                type: 'uint256',
+            },
+        ],
+        stateMutability: 'view',
+        type: 'function',
+        constant: true,
+    },
+    {
+        inputs: [
+            {
+                internalType: 'enum EVoting.Party',
+                name: '',
+                type: 'uint8',
+            },
+        ],
+        name: 'voteCounts',
+        outputs: [
+            {
+                internalType: 'uint256',
+                name: '',
+                type: 'uint256',
+            },
+        ],
+        stateMutability: 'view',
+        type: 'function',
+        constant: true,
+    },
+    {
+        inputs: [
+            {
+                internalType: 'address',
+                name: '',
+                type: 'address',
+            },
+        ],
+        name: 'votes',
+        outputs: [
+            {
+                internalType: 'enum EVoting.Party',
+                name: '',
+                type: 'uint8',
+            },
+        ],
+        stateMutability: 'view',
+        type: 'function',
+        constant: true,
+    },
+    {
+        inputs: [],
+        name: 'startElection',
+        outputs: [],
+        stateMutability: 'nonpayable',
+        type: 'function',
+    },
+    {
+        inputs: [
+            {
+                internalType: 'enum EVoting.Party',
+                name: '_party',
+                type: 'uint8',
+            },
+        ],
+        name: 'vote',
         outputs: [],
         stateMutability: 'nonpayable',
         type: 'function',
     },
     {
         inputs: [],
-        name: 'getValue',
+        name: 'finalizeWinner',
+        outputs: [],
+        stateMutability: 'nonpayable',
+        type: 'function',
+    },
+    {
+        inputs: [],
+        name: 'getWinnerParty',
         outputs: [
             {
-                internalType: 'uint256',
+                internalType: 'string',
                 name: '',
-                type: 'uint256',
+                type: 'string',
             },
         ],
         stateMutability: 'view',
